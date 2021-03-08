a89fa1271bcabd1eb5197216d4d46bd5d953e562
Semion <72217015+SashkaPIX@users.noreply.github.com>
2021-03-08 20:28:41 +0300

    Add files via upload

---
+++ calculatortest/Main.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace calculatortest
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.txtEnterNumbers = new System.Windows.Forms.TextBox();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.lblInfo1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnRadical = new System.Windows.Forms.Button();
+            this.btnDegree = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEnterNumbers
+            // 
+            this.txtEnterNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtEnterNumbers.Location = new System.Drawing.Point(12, 12);
+            this.txtEnterNumbers.Name = "txtEnterNumbers";
+            this.txtEnterNumbers.Size = new System.Drawing.Size(370, 38);
+            this.txtEnterNumbers.TabIndex = 0;
+            this.txtEnterNumbers.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtEnterNumbers.WordWrap = false;
+            this.txtEnterNumbers.TextChanged += new System.EventHandler(this.txtEnterNumbers_TextChanged);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.BackColor = System.Drawing.SystemColors.Control;
+            this.btnMinus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMinus.Location = new System.Drawing.Point(180, 80);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(50, 48);
+            this.btnMinus.TabIndex = 4;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEnter.Location = new System.Drawing.Point(445, 56);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(81, 38);
+            this.btnEnter.TabIndex = 5;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(388, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 38);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "=";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtAnswer.Location = new System.Drawing.Point(427, 12);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(123, 38);
+            this.txtAnswer.TabIndex = 9;
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMultiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnMultiply.Location = new System.Drawing.Point(12, 80);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(50, 48);
+            this.btnMultiply.TabIndex = 1;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDivide.Location = new System.Drawing.Point(68, 80);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(50, 48);
+            this.btnDivide.TabIndex = 3;
+            this.btnDivide.Text = ":";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDevide_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPlus.Location = new System.Drawing.Point(124, 80);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(50, 48);
+            this.btnPlus.TabIndex = 2;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // lblInfo1
+            // 
+            this.lblInfo1.BackColor = System.Drawing.SystemColors.Control;
+            this.lblInfo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblInfo1.Location = new System.Drawing.Point(12, 55);
+            this.lblInfo1.Name = "lblInfo1";
+            this.lblInfo1.Size = new System.Drawing.Size(148, 22);
+            this.lblInfo1.TabIndex = 10;
+            this.lblInfo1.Text = "Enter only numbers";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReset.Location = new System.Drawing.Point(12, 146);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(106, 26);
+            this.btnReset.TabIndex = 11;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnRadical
+            // 
+            this.btnRadical.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRadical.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRadical.Location = new System.Drawing.Point(332, 80);
+            this.btnRadical.Name = "btnRadical";
+            this.btnRadical.Size = new System.Drawing.Size(68, 48);
+            this.btnRadical.TabIndex = 12;
+            this.btnRadical.Text = "√x";
+            this.btnRadical.UseVisualStyleBackColor = true;
+            this.btnRadical.Click += new System.EventHandler(this.btnRadical_Click);
+            // 
+            // btnDegree
+            // 
+            this.btnDegree.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDegree.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDegree.Location = new System.Drawing.Point(258, 80);
+            this.btnDegree.Name = "btnDegree";
+            this.btnDegree.Size = new System.Drawing.Size(68, 48);
+            this.btnDegree.TabIndex = 13;
+            this.btnDegree.Text = "x^2";
+            this.btnDegree.UseVisualStyleBackColor = true;
+            this.btnDegree.Click += new System.EventHandler(this.btnDegree_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 185);
+            this.Controls.Add(this.btnDegree);
+            this.Controls.Add(this.btnRadical);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.lblInfo1);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.txtEnterNumbers);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Main";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEnterNumbers;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Label lblInfo1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnRadical;
+        private System.Windows.Forms.Button btnDegree;
+    }
+}
+
---
+++ calculatortest/Main.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculatortest
+{
+    public partial class Main : Form
+    {
+        // List чисел
+        List<int> listNumbers = new List<int>();
+        // List операторов
+        List<char> listOperators = new List<char>();
+        // Результат
+        double Result;
+        // Значения строки для записи в  listNumbers (Смотреть в txtEnterNumbers)
+        String txtForNumbers;
+        
+        public Main()
+        {
+            InitializeComponent();
+            this.KeyUp += new KeyEventHandler(eventhandlerKeyUp);
+        }
+
+
+        //-----------------------------------------------------------------------------------------------------
+        //------------------------------- Кнопка Enter (Основные вычисления) ----------------------------------
+        //-----------------------------------------------------------------------------------------------------
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            // Поиск умножения 
+            string searchMulty = "*";
+            // MultiplicationAmountount: Количество всех операторов умножить 
+            int i1 = 0, MultiplicationAmountount = 0;
+            while ((i1 = txtEnterNumbers.Text.IndexOf(searchMulty, i1 )) != -1) { ++MultiplicationAmountount; i1 += searchMulty.Length; }
+
+            // Поиск деления
+            string searchDivide = ":";
+            // DivisionAmountount: Количество всех операторов делить
+            int i2 = 0, DivisionAmountount = 0;
+            while ((i2 = txtEnterNumbers.Text.IndexOf(searchDivide, i2 )) != -1) { ++DivisionAmountount; i2 += searchDivide.Length; }
+
+            // Поиск плюса
+            string searchPlus = "+";
+            // AdditionAmountount: Количество всех операторов плюс
+            int i3  = 0, AdditionAmountount = 0;
+            while ((i3 = txtEnterNumbers.Text.IndexOf(searchPlus, i3 )) != -1) { ++AdditionAmountount; i3 += searchPlus.Length; }
+
+            // Поиск минуса
+            string searchMinus = "-";
+            // SubtractionAmountount: Количество всех операторов минус
+            int i4 = 0, SubtractionAmountount = 0;
+            while ((i4 = txtEnterNumbers.Text.IndexOf(searchMinus, i4 )) != -1) { ++SubtractionAmountount; i4 += searchMinus.Length; }
+            
+            // Выполняем все умножения и деления
+            int iDivisionAndMultiplication = 0;
+            while (DivisionAmountount > 0 | MultiplicationAmountount > 0)
+            {
+                if (txtEnterNumbers.Text[iDivisionAndMultiplication] == Convert.ToChar(searchMulty))
+                {
+                    listNumbers[listOperators.IndexOf('*') + 1] = 
+                        listNumbers[listOperators.IndexOf('*')] * 
+                            listNumbers[listOperators.IndexOf('*') + 1];
+
+                    // Удаляем ненужные значения
+                    listNumbers.RemoveAt(listOperators.IndexOf('*'));
+                    listOperators.Remove('*');
+                    MultiplicationAmountount--;
+                }
+                if (txtEnterNumbers.Text[iDivisionAndMultiplication] == Convert.ToChar(searchDivide))
+                {
+                    try
+                    {
+                        listNumbers[listOperators.IndexOf(':') + 1] =
+                            listNumbers[listOperators.IndexOf(':')] /
+                                listNumbers[listOperators.IndexOf(':') + 1];
+                    }
+                    catch(DivideByZeroException)
+                    {
+                        MessageBox.Show("Division by zero", "Error",
+                                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    
+                    // Удаляем ненужные значения
+                    listNumbers.RemoveAt(listOperators.IndexOf(':'));
+                    listOperators.Remove(':');
+                    DivisionAmountount--;
+                }
+                iDivisionAndMultiplication++;
+            }
+
+            // Выполняем все сложения и вычитания 
+            int iPlusAndMinus = 0;
+            while (AdditionAmountount > 0 | SubtractionAmountount > 0)
+            {
+                if (txtEnterNumbers.Text[iPlusAndMinus] == Convert.ToChar(searchPlus))
+                {
+                    // Вычисление
+                    listNumbers[listOperators.IndexOf('+') + 1] =
+                        listNumbers[listOperators.IndexOf('+')] +
+                            listNumbers[listOperators.IndexOf('+') + 1];
+
+                    // Удаляем ненужные значения
+                    listNumbers.RemoveAt(listOperators.IndexOf('+'));
+                    listOperators.Remove('+');
+                    AdditionAmountount--;
+                }
+                if (txtEnterNumbers.Text[iPlusAndMinus] == Convert.ToChar(searchMinus))
+                {
+                    // Вычисление
+                    listNumbers[listOperators.IndexOf('-') + 1] =
+                        listNumbers[listOperators.IndexOf('-')] -
+                            listNumbers[listOperators.IndexOf('-') + 1];
+
+                    // Удаляем ненужные значения
+                    listNumbers.RemoveAt(listOperators.IndexOf('-'));
+                    listOperators.Remove('-');
+                    SubtractionAmountount--;
+                }
+                iPlusAndMinus++;
+            }
+
+            // Записываем последнее число как результат
+            Result = Convert.ToDouble(listNumbers.Last());
+            // Выводим на экран
+            txtAnswer.Text = Convert.ToString(Result);
+            // Фокус на ответ 
+            txtAnswer.Focus();
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        //-------------------------------------------- Ввод текста --------------------------------------------
+        //-----------------------------------------------------------------------------------------------------
+        private void txtEnterNumbers_TextChanged(object sender, EventArgs e)
+        {
+            // Заменяем все операторы на "!" для удобства; записываем новое число после последнего "!";
+            txtForNumbers = txtEnterNumbers.Text.Replace("*", "!").Replace(":", "!").Replace("+", "!").Replace("-", "!");
+            txtForNumbers = txtForNumbers.Substring(txtForNumbers.LastIndexOf("!") + 1);
+            try
+            {
+                listNumbers[listNumbers.Count - 1] = Int32.Parse(txtForNumbers);
+            }
+            catch(FormatException)
+            {
+                return;
+            }
+            
+        }
+
+
+        // ----------------------------- При запуске программы -----------------------------
+        private void Main_Load(object sender, EventArgs e)
+        {
+            listNumbers.Add(new int());
+        }
+        
+
+        //-----------------------------------------------------------------------------------------------------
+        //-------------------------- Кнопки сложения, вычитания, деления, умножения ---------------------------
+        //-----------------------------------------------------------------------------------------------------
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            // Пользователь начинает вводить новое число; Создание нового объекта в listNumbers
+            listNumbers.Add(new int());
+            // Добовляем новый оператор в listNumbers
+            listOperators.Add('*');
+
+            // Если пользователь сам не ввел оператор
+            if (txtEnterNumbers.Text.Last() != '*')
+            {
+                //Выводим на экран
+                txtEnterNumbers.Text += '*';
+            }
+
+        }
+        private void btnDevide_Click(object sender, EventArgs e)
+        {
+            // Пользователь начинает вводить новое число; Создание нового объекта в listNumbers
+            listNumbers.Add(new int());
+            // Добовляем новый оператор в listNumbers
+            listOperators.Add(':');
+
+            // Если пользователь сам не ввел оператор
+            if (txtEnterNumbers.Text.Last() != ':')
+            {
+                //Выводим на экран
+                txtEnterNumbers.Text += ':';
+            }
+        }
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            // Пользователь начинает вводить новое число; Создание нового объекта в listNumbers
+            listNumbers.Add(new int());
+            // Добовляем новый оператор в listNumbers
+            listOperators.Add('+');
+
+            // Если пользователь сам не ввел оператор
+            if (txtEnterNumbers.Text.Last() != '+')
+            {
+                //Выводим на экран
+                txtEnterNumbers.Text += '+';
+            }
+        }
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            // Пользователь начинает вводить новое число; Создание нового объекта в listNumbers
+            listNumbers.Add(new int());
+            // Добовляем новый оператор в listNumbers
+            listOperators.Add('-');
+
+            // Если пользователь сам не ввел оператор
+            if (txtEnterNumbers.Text.Last() != '-')
+            {
+                //Выводим на экран
+                txtEnterNumbers.Text += '-';
+            }
+        }
+
+        //-----------------------------------------------------------------------------------------------------
+        //-------------------------------- Кнопки корня, квадрата, логарифма ----------------------------------
+        //-----------------------------------------------------------------------------------------------------
+        private void btnRadical_Click(object sender, EventArgs e)
+        {
+            Result = Convert.ToDouble( Math.Sqrt(listNumbers[listNumbers.Count - 1]));
+            txtAnswer.Text = Convert.ToString(Result);
+            if (txtAnswer.Text.Length > 6)
+            {
+                txtAnswer.Text = txtAnswer.Text.Remove(6);
+            }
+        }
+        private void btnDegree_Click(object sender, EventArgs e)
+        {
+            Result = Convert.ToDouble(Math.Pow(listNumbers[listNumbers.Count - 1], 2));
+            txtAnswer.Text = Convert.ToString(Result);
+        }
+
+
+        //--------------------------------------------------------------------------------
+        //----------------------------------- Кнопка Reset --------------------------------
+        //--------------------------------------------------------------------------------
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Удаляем все эелементы listNumbers
+            listNumbers.Clear();
+            listOperators.Clear();
+
+            // Удаляем все с экрана
+            txtAnswer.Text = "";
+            txtEnterNumbers.Text = "";
+
+            // Создаем объект для ввода нового числа
+            listNumbers.Add(new int());
+        }
+
+
+        //--------------------------------------------------------------------------------------------
+        //----------------------------------- Обработчик событий KeyUp --------------------------------
+        //--------------------------------------------------------------------------------------------
+        private void eventhandlerKeyUp(object sender, KeyEventArgs e)
+        {
+            // Если пользователь нажал Enter
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnEnter_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+            // Если пользователь нажал умножить
+            if (e.KeyCode == Keys.Multiply)
+            {
+                btnMultiply_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+            if(e.KeyCode == Keys.D8 && (e.Shift))
+            {
+                btnMultiply_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+            
+            // Если пользователь нажал делить
+            if (e.KeyCode == Keys.Divide)
+            {
+                btnDevide_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+            if (e.KeyCode == Keys.OemSemicolon && (e.Shift))
+            {
+                btnDevide_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+            
+            // Если пользователь нажал сложение
+            if (e.KeyCode == Keys.Add)
+            {
+                btnPlus_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+            if (e.KeyCode == Keys.Oemplus)
+            {
+                btnPlus_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }    
+
+            // Если пользователь нажал минус
+            if (e.KeyCode == Keys.Subtract)
+            {
+                btnMinus_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+            if (e.KeyCode == Keys.OemMinus)
+            {
+                btnMinus_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+
+            // Если пользователь нажал Backspace
+            if (e.KeyCode == Keys.Back)
+            {
+                btnReset_Click(null, null);
+                // Прекращаем выполнение eventhandlerKeyUp
+                return;
+            }
+        }
+    }
+}
